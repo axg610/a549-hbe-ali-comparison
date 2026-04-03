e41317e0c5081ec44c5c90b1f4f70648492f1989
Alex <alexg@alex17.localdomain>
2026-04-02 22:34:26 -0600

    fix helper color labels

--- modules/helpers.R
+++ modules/helpers.R
@@ -1,9 +1,8 @@
 myColors <- c(
   "NS" = "grey30", 
   "IL1B" = "#FC4E07", 
-  "Bud" = "#E7B800", 
-  "I+B" = "#2E9FDF",
-  "IB" = "#2E9FDF"
+  "GC" = "#E7B800", 
+  "combo" = "#2E9FDF"
 )
 
 cleanup_objects <- function(keep = readLines("cleanup_ignore.txt")) {
