7062d0ee4d7c929cea07a5659c5664d67c4e0e80
Alex <alexg@alex17.localdomain>
2026-04-08 19:12:30 -0600

    moved generation of upset plots to a helper function

--- modules/helpers.R
+++ modules/helpers.R
@@ -11,3 +11,44 @@ cleanup_objects <- function(keep = readLines("cleanup_ignore.txt")) {
   rm(list = to_remove, envir = .GlobalEnv)
   # gc()
 }
+
+plot_upset <- function(logicalMatrix, minSize = 50, plotTitle = "title here"){
+  
+  # a wrapper for the ComplexUpset::upset function that applies my custom theme
+  # to create an UpSet plot given a logical matrix specifying samples as colnames,
+  # presence/absence as 1/0, and gene names as rownames.
+  
+  suppressWarnings(
+    upset(
+      logicalMatrix,
+      intersect = colnames(logicalMatrix),
+      min_size = minSize,
+      set_sizes = F,
+      height_ratio = 0.75,
+      wrap = T,
+      base_annotations = list(
+        "intersection size" = intersection_size(
+          text = list(
+            color = "black",
+            angle = 90, 
+            vjust = 0.5,
+            hjust = -0.3,
+            size = 3
+          )
+        )
+      )) +
+      theme_minimal() +
+      theme(
+        axis.text.x = element_blank(),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        panel.grid.minor = element_blank(),
+        plot.title = element_text(size = 20, hjust = 0.05)
+      ) +
+      labs(title = plotTitle)
+  )
+}
+
+
+
+
