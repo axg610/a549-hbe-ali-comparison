63b56a8b657167be80a38e51fbc1a6d1b7cd3e19
Alex <alexg@alex17.localdomain>
2026-04-14 14:40:05 -0600

    added fold and FDR thresholded venns

--- modules/fig-old-venns.R
+++ modules/fig-old-venns.R
@@ -42,8 +42,39 @@ downreg <- dea %>%
   pivot_wider(names_from = "celltype", values_from = "value", values_fill = 0) %>%
   column_to_rownames("Gene")
 
+reg_fold_only <- dea %>%
+  filter(
+    treatment == "IL1B",
+    abs(log2fold) >= 1
+  ) %>%
+  group_by(celltype) %>%
+  summarize(Gene = list(unique(Gene)), .groups = "drop") %>%
+  unnest(Gene) %>%
+  mutate(value = 1) %>%
+  pivot_wider(names_from = "celltype", values_from = "value", values_fill = 0) %>%
+  column_to_rownames("Gene")
+
+reg_fdr_only <- dea %>%
+  filter(
+    treatment == "IL1B",
+    FDR <= 0.05
+  ) %>%
+  group_by(celltype) %>%
+  summarize(Gene = list(unique(Gene)), .groups = "drop") %>%
+  unnest(Gene) %>%
+  mutate(value = 1) %>%
+  pivot_wider(names_from = "celltype", values_from = "value", values_fill = 0) %>%
+  column_to_rownames("Gene")
+
 # plot
 
-plot_euler(reg, plotTitle = "(A) IL1B-regulated genes at 6h") %>% print()
-plot_euler(upreg, plotTitle = "(B) IL1B-upregulated genes at 6h") %>% print()
-plot_euler(downreg, plotTitle = "(C) IL1B-downregulated genes at 6h") %>% print()
+plot_euler(reg, plotTitle = "A. regulated") %>% print()
+plot_euler(upreg, plotTitle = "B. upregulated") %>% print()
+plot_euler(downreg, plotTitle = "C. downregulated") %>% print()
+
+plot_euler(reg_fold_only, plotTitle = "D. regulated, fold threshold only") %>% print()
+plot_euler(reg_fdr_only, plotTitle = "E. regulated, FDR threshold only") %>% print()
+
+
+
+
